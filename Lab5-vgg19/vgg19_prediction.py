ea087a4f703d36b829deee0e007720e460297217
bigballon <605718199@qq.com>
2017-04-06 23:37:11 +0800

    os.path.exists(file_path)

--- Lab5-vgg19/vgg19_prediction.py
+++ Lab5-vgg19/vgg19_prediction.py
@@ -4,16 +4,29 @@ from keras.applications.vgg19 import preprocess_input, decode_predictions
 from PIL import Image
 from keras.preprocessing import image
 import numpy as np
+import os.path
 
 model = VGG19(weights='imagenet')
 
 while True:
-	img_path = input('Please input picture file to predict ( input Q to exit ):')
+	img_path = input('Please input picture file to predict ( input Q to exit ):  ')
 	if img_path == 'Q':
 		break
-	img = image.load_img(img_path, target_size=(224, 224))
-	x = image.img_to_array(img)
-	x = np.expand_dims(x, axis=0)
-	x = preprocess_input(x)
-	results = model.predict(x)
-	print('Predicted:', decode_predictions(results, top=5)[0])
+	if not os.path.exists(img_path):
+		print("file not exist!")
+		continue
+	try:
+		img = image.load_img(img_path, target_size=(224, 224))
+		x = image.img_to_array(img)
+		x[:, :, 0] = x[:, :, 0] - 103.939
+		x[:, :, 1] = x[:, :, 1] - 116.779
+		x[:, :, 2] = x[:, :, 2] - 123.680
+		x = np.expand_dims(x, axis=0)
+		# x = preprocess_input(x)
+		results = model.predict(x)
+		print('Predicted:', decode_predictions(results, top=5)[0])
+	except Exception as e:
+		pass
+
+
+	
